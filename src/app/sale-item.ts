ab7337a19ea39872820049437ba7abaed03c3b7a
Shung <jordan880805@gmail.com>
2018-12-05 12:52:16 +0800

    trace

--- src/app/sale-item.ts
+++ src/app/sale-item.ts
@@ -2,6 +2,7 @@ export interface SaleItem {
     id: number;
     sale_id: number;
     product_id: number;
+    // porduct_name: string;
     quantity: number;
     sale_price: number;
     created_at: string;
