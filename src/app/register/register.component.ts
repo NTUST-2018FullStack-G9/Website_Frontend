a54fd5a13681c5c953fbade91cac8dce3e67ee87
ericzone <eric885251@gmail.com>
2018-12-05 13:40:58 +0800

    register 密碼重複驗證跳訊息

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,31 +14,42 @@ export class RegisterComponent implements OnInit {
     password: '',
     password_confirmation: ''
   };
-  hero;
+  passwordH;
   constructor(private memberService: MemberService, private router: Router) {}
 
-  ngOnInit() {}
-  register() {
-
-    if (this.user.password.length >= 6 && (this.user.password === this.user.password_confirmation)) {
-      console.log(this.user);
-      this.memberService.register(this.user).subscribe(
-      (data: any) => {
-        console.log(data);
-        if (data.success) {
-          this.router.navigate(['/login']);
-        } else {
-          alert('fail');
+  ngOnInit() {
+    this.passwordH = true;
+  }
+  register(type) {
+    this.passwordH = true;
+    if (type) {
+      console.log('true');
+      if (this.user.password === this.user.password_confirmation) {
+        console.log(this.user);
+        this.memberService.register(this.user).subscribe(
+        (data: any) => {
+          console.log(data);
+          if (data.success) {
+            this.router.navigate(['/login']);
+          } else {
+            alert('fail');
+          }
+        },
+        response => {
+          console.log(response);
+          alert(response.error.message);
         }
-      },
-      response => {
-        console.log(response);
-        alert(response.error.message);
+      );
+      } else {
+        this.passwordH = false;
       }
-    );
-    }
+    } else {
+      console.log('false');
+      if (this.user.password !== this.user.password_confirmation) {
+        this.passwordH = false;
+      }
     }
 
-
+  }
 }
 
