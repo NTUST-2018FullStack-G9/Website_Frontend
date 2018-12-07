d2106aeeff536b10abe1d4acb24a07f421ff0689
Shung <jordan880805@gmail.com>
2018-12-07 20:39:42 +0800

    一對垃圾臭蟲

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -20,38 +20,85 @@ export class ProductDetailComponent implements OnInit {
   getImage(Imgname) {
     return this.dataService.getImage(Imgname);
   }
+
   addCarts(item: Product) {
     this.isIn = false;
-    this.cartService.addToCarts(item).subscribe(
-      data => {
-        for (const i of this.cartService.cartsInService) {
-          if (i.product_id === item.id) {
-            i.quantity += this.quantity;
-            this.isIn = true;
+    for (const i of this.cartService.cartsInService) {
+      if (i.product_id === item.id) {
+        for (let j = 0 ; j < this.quantity + 1 ; j++) {
+          this.cartService.addCarts(i).subscribe( data => {
+            console.log(data);
+          } , (response) => {
+            console.log(response);
           }
+          );
         }
-        if (!this.isIn) {
-          this.cartService.cartsInService.push({
-            id: 0,
-            member_id: 0,
-            product_id: item.id,
-            quantity: this.quantity,
-            price: item.saleprice,
-            product_name: item.name,
-            created_at: '',
-            updated_at: '',
-            product_imagename: item.imagename,
-          });
-        }
-        console.log(item);
-        console.log(data);
-        alert('addCarts');
-      },
-      response => {
-        console.log(response);
+        i.quantity += this.quantity;
+        this.isIn = true;
+        alert('add');
+        break;
       }
-    );
+    }
+    if (!this.isIn) {
+      this.cartService.addToCarts(item).subscribe(
+        data => {
+          if (!this.isIn) {
+            this.cartService.cartsInService.push({
+              id: 0,
+              member_id: 0,
+              product_id: item.id,
+              quantity: this.quantity,
+              price: item.saleprice,
+              product_name: item.name,
+              created_at: '',
+              updated_at: '',
+              product_imagename: item.imagename,
+            });
+          }
+          console.log(item);
+          console.log(data);
+          alert('addCarts');
+        },
+        response => {
+          console.log(response);
+        }
+      );
+    }
   }
+
+  // addCarts(item: Product) {
+  //   this.isIn = false;
+  //   this.cartService.addToCarts(item).subscribe(
+  //     data => {
+  //       for (const i of this.cartService.cartsInService) {
+  //         if (i.product_id === item.id) {
+  //           i.quantity += this.quantity;
+  //           this.isIn = true;
+  //         }
+  //       }
+  //       if (!this.isIn) {
+  //         this.cartService.cartsInService.push({
+  //           id: 0,
+  //           member_id: 0,
+  //           product_id: item.id,
+  //           quantity: this.quantity,
+  //           price: item.saleprice,
+  //           product_name: item.name,
+  //           created_at: '',
+  //           updated_at: '',
+  //           product_imagename: item.imagename,
+  //         });
+  //       }
+  //       console.log(item);
+  //       console.log(data);
+  //       alert('addCarts');
+  //     },
+  //     response => {
+  //       console.log(response);
+  //     }
+  //   );
+  // }
+
   ngOnInit() {
     this.quantity = 1;
     this.route.params.subscribe(data => {
