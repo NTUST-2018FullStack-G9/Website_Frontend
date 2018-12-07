32285a5b40ffbb2654a2530b848d0a7449b3c646
Shung <jordan880805@gmail.com>
2018-12-07 16:38:40 +0800

    coupon

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -13,26 +13,70 @@ import { CouponService } from '../coupon.service';
 })
 export class CheckoutComponent implements OnInit {
   check;
-  couponCode;
+  price = 0;
+  IsCoupon = false;
+  IsClick = false;
+
   member = {
-    member_id : 0,
-    order_name : '',
-    order_phone : '',
-    order_address : '',
-    order_note : '',
+    member_id: 0,
+    order_name: '',
+    order_phone: '',
+    order_address: '',
+    order_note: ''
+  };
+  couponCode = {
+    type: 0,
+    code: ''
   };
 
   get carts() {
     return this.cartService.cartsInService;
   }
-  constructor( private cartService: CartService,
-     private router: Router,
-     private memberService: MemberService,
-     private couponService: CouponService,
-     ) {
+
+  get IsDP() {
+    return this.IsCoupon;
   }
 
-  price = 0;
+  constructor(private cartService: CartService, private router: Router, private memberService: MemberService, private couponService: CouponService) {}
+
+  // applyCoupon() {
+  //   console.log();
+  //   this.couponService.applyCoupon(this.couponCode.code).subscribe( data => {
+  //     console.log(data);
+  //   }, (response) => {
+  //     console.log(response);
+  //   });
+
+  // }
+
+  applyCoupon() {
+    this.couponService.applyCoupon(this.couponCode.code).subscribe(
+      data => {
+        console.log(data);
+      },
+      response => {
+        console.log(response);
+      }
+    );
+
+    this.IsClick = true;
+    if (this.IsDP) {
+      alert('Dont try to get Dp again you stupid');
+    } else {
+      if (this.couponCode.code === '6666' || this.couponCode.code === '5487') {
+        alert('20% off');
+        this.IsCoupon = true;
+        for (const i of this.carts) {
+          i.price = i.price * 0.8;
+        }
+      } else if (this.couponCode.code === '') {
+        this.IsClick = false;
+      } else {
+        this.IsCoupon = false;
+      }
+    }
+  }
+
   getCartprice() {
     this.price = 0;
     for (const i of this.cartService.cartsInService) {
@@ -43,27 +87,21 @@ export class CheckoutComponent implements OnInit {
 
   checkout() {
     // alert('checkOut');
-    this.cartService.createOrder(this.member).subscribe(data => {
-      console.log(data);
-      alert('checkout success');
-      this.router.navigate(['/']);
-      location.reload();
-    }, (response) => {
-      if (response.status === 401) {
-        // alert('please login first');
-        this.router.navigate(['/login']);
+    this.cartService.createOrder(this.member).subscribe(
+      data => {
+        console.log(data);
+        alert('checkout success');
+        this.router.navigate(['/']);
+        location.reload();
+      },
+      response => {
+        if (response.status === 401) {
+          // alert('please login first');
+          this.router.navigate(['/login']);
+        }
+        console.log(response);
       }
-      console.log(response);
-    });
-
-  }
-
-  applyCoupon() {
-    this.couponService.applyCoupon(this.couponCode).subscribe( data => {
-      console.log(data);
-    }, (response) => {
-      console.log(response);
-    });
+    );
   }
 
   ngOnInit() {
@@ -74,5 +112,4 @@ export class CheckoutComponent implements OnInit {
       console.log(this.member.member_id);
     });
   }
-
 }
