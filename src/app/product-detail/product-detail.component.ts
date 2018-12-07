9cec8a187bc49e8ae6522143c9d9d431fc17cd28
Shung <jordan880805@gmail.com>
2018-12-07 22:41:05 +0800

    new api

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -24,6 +24,27 @@ export class ProductDetailComponent implements OnInit {
     this.cartService.addNumInCarts(item).subscribe(
       data => {
         console.log(data);
+        alert('add to cart');
+        for (const i of this.cartService.cartsInService) {
+          if (i.product_id === item.id) {
+            i.quantity += this.quantity;
+            this.isIn = true;
+            break;
+          }
+        }
+        if (!this.isIn) {
+          this.cartService.cartsInService.push({
+            id: 0,
+            member_id: 0,
+            product_id: item.id,
+            quantity: this.quantity,
+            price: item.saleprice,
+            product_name: item.name,
+            created_at: '',
+            updated_at: '',
+            product_imagename: item.imagename
+          });
+        }
       },
       response => {
         console.log(response);
@@ -31,59 +52,59 @@ export class ProductDetailComponent implements OnInit {
     );
   }
 
-  addCarts(item: Product) {
-    console.log(this.quantity);
-    this.isIn = false;
-    for (const i of this.cartService.cartsInService) {
-      if (i.product_id === item.id) {
-        alert(this.quantity);
-        for (let j = 0; j < this.quantity; j++) {
-          setTimeout(() => {
-            console.log('j', j);
-            this.cartService.addCarts(i).subscribe(
-              data => {
-                setTimeout(() => {
-                  console.log(data);
-                }, 500);
-              },
-              response => {
-                console.log(response);
-              }
-            );
-          }, 500);
-        }
-        i.quantity += this.quantity;
-        this.isIn = true;
-        alert('add');
-        break;
-      }
-    }
-    if (!this.isIn) {
-      this.cartService.addToCarts(item).subscribe(
-        data => {
-          if (!this.isIn) {
-            this.cartService.cartsInService.push({
-              id: 0,
-              member_id: 0,
-              product_id: item.id,
-              quantity: this.quantity,
-              price: item.saleprice,
-              product_name: item.name,
-              created_at: '',
-              updated_at: '',
-              product_imagename: item.imagename
-            });
-          }
-          console.log(item);
-          console.log(data);
-          alert('addCarts');
-        },
-        response => {
-          console.log(response);
-        }
-      );
-    }
-  }
+  // addCarts(item: Product) {
+  //   console.log(this.quantity);
+  //   this.isIn = false;
+  //   for (const i of this.cartService.cartsInService) {
+  //     if (i.product_id === item.id) {
+  //       alert(this.quantity);
+  //       for (let j = 0; j < this.quantity; j++) {
+  //         setTimeout(() => {
+  //           console.log('j', j);
+  //           this.cartService.addCarts(i).subscribe(
+  //             data => {
+  //               setTimeout(() => {
+  //                 console.log(data);
+  //               }, 500);
+  //             },
+  //             response => {
+  //               console.log(response);
+  //             }
+  //           );
+  //         }, 500);
+  //       }
+  //       i.quantity += this.quantity;
+  //       this.isIn = true;
+  //       alert('add');
+  //       break;
+  //     }
+  //   }
+  //   if (!this.isIn) {
+  //     this.cartService.addToCarts(item).subscribe(
+  //       data => {
+  //         if (!this.isIn) {
+  //           this.cartService.cartsInService.push({
+  //             id: 0,
+  //             member_id: 0,
+  //             product_id: item.id,
+  //             quantity: this.quantity,
+  //             price: item.saleprice,
+  //             product_name: item.name,
+  //             created_at: '',
+  //             updated_at: '',
+  //             product_imagename: item.imagename
+  //           });
+  //         }
+  //         console.log(item);
+  //         console.log(data);
+  //         alert('addCarts');
+  //       },
+  //       response => {
+  //         console.log(response);
+  //       }
+  //     );
+  //   }
+  // }
 
   // addCarts(item: Product) {
   //   this.isIn = false;
